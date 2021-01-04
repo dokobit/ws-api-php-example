1fc4628feca8b085f9d005ccfb447ac4f5533b1f
epelanis <70586563+epelanis@users.noreply.github.com>
2021-01-04 09:54:52 +0200

    Update mobile_sign.php

--- mobile_sign.php
+++ mobile_sign.php
@@ -4,8 +4,11 @@
     $url = 'https://developers.dokobit.com';
     $accessToken = ''; //Enter valid developer access token here.
     $file = isset($argv[1])?$argv[1]:'./test.pdf';
-    $phone = isset($argv[2])?$argv[2]:'+37060000007';
-    $code = isset($argv[3])?$argv[3]:'51001091072';
+    /**
+    * Mobile ID test data can be found at https://support.dokobit.com/article/667-mobile-id-and-smart-id-test-data
+    */
+    $phone = isset($argv[2])?$argv[2]:'+37060000666';
+    $code = isset($argv[3])?$argv[3]:'50001018865';
 
     if (empty($accessToken)) {
         echo "Access Token is required. Enter at line 5.\n";
