ab63b246c458903ffafc9b2d0fa69f4e845f1fbc
epelanis <70586563+epelanis@users.noreply.github.com>
2021-01-06 12:12:34 +0200

    Add files via upload

---
+++ mobile_login.php
@@ -0,0 +1,42 @@
+<?php
+    include './lib.php';
+    echo "developers.dokobit.com WS API Mobile ID login PHP example\n";
+    $url = 'https://developers.dokobit.com';
+    $accessToken = ''; //Enter valid developer access token here.
+    $phone = isset($argv[2])?$argv[2]:'+37060000666';
+    $code = isset($argv[3])?$argv[3]:'50001018865';
+
+    if (empty($accessToken)) {
+        echo "Access Token is required. Enter at line 5.\n";
+        exit;
+    }
+
+    echo "Requesting login:\n";
+    $prepared = request($url, $accessToken, 'v2/mobile/login', [
+        'code' => $code,
+        'phone' => $phone
+    ]);
+    echo "Responded: [".$prepared['status']."]\n";
+
+    if ($prepared['status'] != 'ok') {
+        print_r($prepared);
+        exit;
+    }
+    echo "Token: [ " . $prepared['token'] . " ]\n";
+    echo "Your phone will receive Mobile ID authentication request with\nVerification code: [ " . $prepared['control_code'] . " ]\n";
+
+    echo "Requesting status:\n";
+    $time = 120;
+    while ($time > 0) {
+        $statusResponse = request($url, $accessToken, 'v2/mobile/login/status/' . $prepared['token'], [], false);
+        echo "Status: [".$statusResponse['status']."]\n";
+        if ($statusResponse['status'] == 'ok') {
+            print_r($statusResponse);
+            exit;
+        } elseif ($statusResponse['status'] == 'error') {
+            var_dump($statusResponse['message']);
+            exit;
+        }
+        sleep(2);
+        $time -=2;
+    }
