32a1983ce6b9758970bf79c27e207412a1fcc814
Edgaras Pelanis <70586563+epelanis@users.noreply.github.com>
2021-02-17 12:29:26 +0200

    Update mobile_sign.php

--- mobile_sign.php
+++ mobile_sign.php
@@ -6,9 +6,6 @@
     $file = isset($argv[1])?$argv[1]:'./test.pdf';
     $phone = isset($argv[2])?$argv[2]:'+37060000666';
     $code = isset($argv[3])?$argv[3]:'50001018865';
-    /**
-    * Mobile ID test data can be found at https://dokobit.com/developers/testing
-    */
 
     if (empty($accessToken)) {
         echo "Access Token is required. Enter at line 5.\n";
