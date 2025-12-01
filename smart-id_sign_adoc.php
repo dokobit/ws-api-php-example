656cf1cc68be46ed3b408dce0e5450dcc98d638f
Edgaras Pelanis <70586563+epelanis@users.noreply.github.com>
2025-12-01 13:22:07 +0200

    Update smart-id_sign_adoc.php

--- smart-id_sign_adoc.php
+++ smart-id_sign_adoc.php
@@ -6,7 +6,7 @@
     $country = isset($argv[1])?$argv[1]:'lt';
     $code = isset($argv[2])?$argv[2]:'30303039914';
     /**
-    * Smart-ID test data can be found at https://support.dokobit.com/article/667-mobile-id-and-smart-id-test-data
+    * Smart-ID test data can be found at https://dokobitbysignicat.zendesk.com/hc/en-us/articles/20067528968476-Mobile-ID-and-Smart-ID-test-data
     */
     $file = isset($argv[3])?$argv[3]:'./test.pdf';
 
